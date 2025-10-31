7747b9f0935b9e19bfeca075d34635b531199663
Aleksandr Sokolov <sokolovmono@gmail.com>
2025-10-31 21:35:51 +0300

    Add Knexfile for setup

---
+++ knexfile.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+
+export default {
+    development: {
+        client: 'pg',
+        connection: {
+            host: process.env.PG_HOST,
+            port: process.env.PG_PORT,
+            user: process.env.PG_USER,
+            password: process.env.PG_PASSWORD,
+            database: process.env.PG_DB,
+        },
+        migrations: {
+            directory: './src/migrations',
+        },
+    },
+};
